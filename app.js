ec5108599779a9fa288cf37cd48028f051af5640
Ivan Dimov <ivan.dmvv@gmail.com>
2019-07-09 13:57:39 +0300

    update method start server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+require('./db');
+
 const Koa = require('koa');
-const db = require('./db');
 const routes = require('./routes');
 const koaBody = require('koa-body');
 const config = require('./config.json');
@@ -10,8 +11,6 @@ app.use(koaBody());
 app.use(routes.routes());
 app.use(routes.allowedMethods());
 
-db.initDb(() => {
-    app.listen(config.app.port, () => {
-        console.log(`Application is running here: http://${config.host}:${config.app.port}`);
-    });
+app.listen(config.app.port, () => {
+    console.log(`Application is running here: http://${config.host}:${config.app.port}`);
 });
--- db/index.js
+++ db/index.js
@@ -3,30 +3,25 @@ const config = require('../config.json');
 
 mongoose.Promise = global.Promise;
 
-const initDb = (listen) => {
-    const connectionURL = `mongodb://${config.host}:${config.db.port}/${config.db.name}`;
+const connectionURL = `mongodb://${config.host}:${config.db.port}/${config.db.name}`;
 
-    mongoose.connect(connectionURL, {useNewUrlParser: true}).catch(err => console.log(err));
+mongoose.connect(connectionURL, {useNewUrlParser: true}).catch(err => console.log(err));
 
-    const db = mongoose.connection;
+const db = mongoose.connection;
 
-    db.on('connected', () => {
-        listen();
-        console.log(`Mongoose connection open on ${connectionURL}`);
+db.on('connected', () => {
+    console.log(`Mongoose connection open on ${connectionURL}`);
+});
+
+db.on('error', err => console.log(err));
+
+db.on('disconnected', () => {
+    console.log('Mongoose connection disconnected');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log('Mongoose connection closed');
+        process.exit(0);
     });
-
-    db.on('error', err => console.log(err));
-
-    db.on('disconnected', () => {
-        console.log('Mongoose connection disconnected');
-    });
-
-    process.on('SIGINT', () => {
-        db.close(() => {
-            console.log('Mongoose connection closed');
-            process.exit(0);
-        });
-    });
-};
-
-module.exports = { initDb };
+});
